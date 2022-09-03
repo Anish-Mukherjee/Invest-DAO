bf7df6d9c861756f7cdb987eed7abf97256940bd
Anish <cybertronian217@gmail.com>
2022-09-03 17:24:04 +0530

    CSS fixes

--- components/Login.js
+++ components/Login.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import {useMetamask} from '@thirdweb-dev/react'
+import React from "react";
+import { useMetamask } from "@thirdweb-dev/react";
 
 const Login = () => {
-    const connectWithMetamask = useMetamask() 
+  const connectWithMetamask = useMetamask();
   return (
     <div>
-      <button onClick={connectWithMetamask} className="bg-gradient-to-r from-fuchsia-600 to-pink-600 p-5 text-xl font-bold text-white rounded-full">Sign in with Metamask</button>
+      <button
+        onClick={connectWithMetamask}
+        className="bg-gradient-to-r from-fuchsia-600 to-pink-600 p-5 text-xl font-bold text-white rounded-full"
+      >
+        Sign in with Metamask
+      </button>
+      <p className="text-white pt-9 hover:underline">
+        <a
+          href="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?hl=en"
+          target="_blank"
+        >
+          No metamask? Download from here
+        </a>
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
